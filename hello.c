62d9f2044f58190c943dc108df115a3c0d24b2a6
JXC3149 <107070493+JXC3149@users.noreply.github.com>
2024-11-10 22:48:54 -0600

    Added team name and member names.

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Hello, World!\nWe are Team 1 (Team Black), and our names are:\n-Person 1\n-Person 2\n-Person 3\n-Person 4\n");
     return 0;
 }
